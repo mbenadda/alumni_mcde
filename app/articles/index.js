e7b0ff3e0bd4cc1b55779ed8bc1d6e647359c368
Mehdi Benadda <me@mbenadda.com>
2014-12-04 23:28:33 +0100

    Boilerplate for the whole article part of the API

---
+++ app/articles/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+
+var router = express.Router();
+
+// Get a list of articles
+router.get('/', require('get_list'));
+
+// If the article parameter is present, use this middleware
+router.param('article', function (req, res, next, id) {
+
+});
+
+// Get a single article
+router.get('/:article', require('get_single'));
+
+// Verify that the requesting user is authorized before going any further
+router.use(function (req, res, next) {
+
+});
+
+// Create a new article
+router.post('/', require('create'));
+
+// Edit an existing article
+router.put('/:article', require('edit'));
+
+module.exports = router;
