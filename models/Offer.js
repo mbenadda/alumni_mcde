ce66c63b5de0d2657b7c810bea3b862ae76e6e17
Mehdi Benadda <me@mbenadda.com>
2014-12-07 00:29:13 +0100

    Added model for Offers

---
+++ models/Offer.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var Offer = new Schema({
+  data: {
+    title: { type: String, required: true, index: true },
+    tags: [ { type: String } ],
+    company: { type: String, required: true },
+    description: { type: String, required: true }
+  },
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  created: { type: Date, default: Date.now },
+  lastupdate: { type: Date, default: Date.now }
+})
+
+module.exports = {
+  schema: Offer,
+  model: mongoose.model('Offer', Offer)
+}
