0f7a95eaff79cacf293a83789b43c6012f7b5d4e
Mehdi Benadda <me@mbenadda.com>
2014-12-07 18:51:49 +0100

    Added Winston to handle logging (console & files based)

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
-var morgan = require('morgan');
+var logger  = require('./lib/logger');
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
-app.use(morgan('dev')); // Logs requests to console
+app.use(require('morgan')('default', { stream: logger.steam })); // Pass express' logs to our logger
+
+// Connect to the database and get an instance of the connection
+var db = require('./lib/db')();
 
 // Decentralize routing to a separate app folder and pass app as that's what the routes are bound to
 var routes = require('./app')(app);
